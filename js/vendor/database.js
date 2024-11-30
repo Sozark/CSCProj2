3c0cc197f525cfc25d745d7f4d7405336344e44f
Sozark <121688154+Sozark@users.noreply.github.com>
2024-11-29 21:29:54 -0600

    Schema Created

--- js/vendor/database.js
+++ js/vendor/database.js
@@ -2,6 +2,30 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/parking', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/parking', { 
+     useNewUrlParser: true,
+     useUnifiedTopology: true,  
+ });
 
-const parking
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error')); 
+db.once('open', () => {
+    console.log('Cononected to MongoDB'); 
+});
+
+module.exports = mongoose; 
+
+//This section is the creation of a schema for the data 
+
+const mongoose = require('mongoose');
+
+const parkingSchema = new mongoose.Schema({
+    lotName: String, 
+    availablility: String, 
+    day: String, 
+}); 
+
+const ParkingLot = mongoose.model('ParkingLot', parkingSchema); 
+
+module.exports = ParkingLot; 
